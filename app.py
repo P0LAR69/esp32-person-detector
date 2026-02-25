f3b812382b2d7550b1ee054118c9c5e439611718
Jonathan David Cabrera <116048885+P0LAR69@users.noreply.github.com>
2026-02-25 16:19:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
-from PIL import Image
-import os
+import base64
+import numpy as np
+import cv2
 
 app = Flask(__name__)
-
-# Cargar modelo nano (ligero)
 model = YOLO("yolov8n.pt")
 
 @app.route("/")
@@ -14,28 +13,23 @@ def home():
 
 @app.route("/detect", methods=["POST"])
 def detect():
-    if "image" not in request.files:
-        return jsonify({"error": "No image sent"}), 400
+    data = request.json
+    image_base64 = data["image"]
 
-    file = request.files["image"]
-    img = Image.open(file.stream)
-
-    # Redimensionar para hacerlo más ligero
-    img = img.resize((320, 320))
+    # Decodificar base64
+    image_bytes = base64.b64decode(image_base64)
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
     results = model(img)
 
     persons = 0
-
     for r in results:
         for box in r.boxes:
-            cls = int(box.cls[0])
-            label = model.names[cls]
-            if label == "person":
+            if int(box.cls) == 0:  # clase 0 = persona
                 persons += 1
 
     return jsonify({"persons_detected": persons})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
